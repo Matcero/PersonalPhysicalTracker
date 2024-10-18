a2ee6eb47cf75ba31a10fcdcbc78df06cca7c288
Matcero <103655455+Matcero@users.noreply.github.com>
2024-10-18 16:23:19 +0200

    Root per calendario

--- LabMob/src/app/calendar/calendar.page.ts
+++ LabMob/src/app/calendar/calendar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Importa il Router
 
 @Component({
   selector: 'app-calendar',
@@ -7,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     // Initialize any required data
   }
+
+  // Funzione per andare alla home
+      goToHome() {
+        this.router.navigate(['/home']); // Naviga verso la home
+      }
+
+    // Add a method to navigate to the calendar
+    goToCalendar() {
+      this.router.navigate(['/calendar']);
+    }
 
 }
